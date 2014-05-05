677cf1e4766b316f566c8c603bffb1d59f1ccbf9
Adel Gadllah <adel.gadllah@gmail.com>
2014-05-05 11:27:36 +0200

    Add mongo config

--- cloudSimulator/src/main/java/cloudSimulator/Simulator.java
+++ cloudSimulator/src/main/java/cloudSimulator/Simulator.java
@@ -2,11 +2,13 @@ package cloudSimulator;
 
 import java.net.URL;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 import simulation.ElasticityManager;
 
@@ -20,6 +22,9 @@ public class Simulator implements CommandLineRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(Simulator.class, args);
 	}
+	
+	@Autowired
+	MongoTemplate tpl;
 
 	public void run(String... arg0) throws Exception {
 		System.out.println("Started");
@@ -31,8 +36,10 @@ public class Simulator implements CommandLineRunner {
 		ElasticityManager em = new ElasticityManager();
 		em.setDataCenters(parser.getDataCenters());
 		
+		System.out.println(tpl);
+		
 		for (int i = 0; i < simulatedMinutes; i++) {
-			em.simulate();
+			//em.simulate();
 		}
 
 		System.out.println("Simulation ended!");
