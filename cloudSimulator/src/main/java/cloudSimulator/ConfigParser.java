a847776ab982d097961b8051cfa4affb6eff2934
Adel Gadllah <adel.gadllah@gmail.com>
2014-05-05 10:35:56 +0200

    configParser: Implement hardware params

--- cloudSimulator/src/main/java/cloudSimulator/ConfigParser.java
+++ cloudSimulator/src/main/java/cloudSimulator/ConfigParser.java
@@ -26,10 +26,16 @@ public class ConfigParser {
 		
 		NormalDistribution pmND = new NormalDistribution(Double.parseDouble(ini.get("PMs", "mean")), Double.parseDouble(ini.get("PMs", "sd")));
 		NormalDistribution vmND = new NormalDistribution(Double.parseDouble(ini.get("VMs", "mean")), Double.parseDouble(ini.get("VMs", "sd")));
+
 		NormalDistribution cpuPowerND = new NormalDistribution(Double.parseDouble(ini.get("CPUPower", "mean")), Double.parseDouble(ini.get("CPUPower", "sd")));
 		NormalDistribution memPowerND = new NormalDistribution(Double.parseDouble(ini.get("MemoryPower", "mean")), Double.parseDouble(ini.get("MemoryPower", "sd")));
 		NormalDistribution netPowerND = new NormalDistribution(Double.parseDouble(ini.get("NetworkPower", "mean")), Double.parseDouble(ini.get("NetworkPower", "sd")));
-		
+
+		NormalDistribution cpuCoresND = new NormalDistribution(Double.parseDouble(ini.get("CPUCores", "mean")), Double.parseDouble(ini.get("CPUCores", "sd")));
+		NormalDistribution memoryND = new NormalDistribution(Double.parseDouble(ini.get("Memory", "mean")), Double.parseDouble(ini.get("Memory", "sd")));
+		NormalDistribution bandwithND = new NormalDistribution(Double.parseDouble(ini.get("Bandwidth", "mean")), Double.parseDouble(ini.get("Bandwidth", "sd")));
+		NormalDistribution diskspaceND = new NormalDistribution(Double.parseDouble(ini.get("Diskspace", "mean")), Double.parseDouble(ini.get("Diskspace", "sd")));
+
 		
 		for (String key : ini.get("DataCenter").keySet()) {
 			DataCenter dc = new DataCenter();
@@ -40,10 +46,16 @@ public class ConfigParser {
 			List <PhysicalMachine> pms = new ArrayList<PhysicalMachine>();
 			for (int i = 0; i < numPMs; i++) {
 				PhysicalMachine pm = new PhysicalMachine();
+				pm.setCpus((int)cpuCoresND.sample());
+				pm.setMemory((int)memoryND.sample());
+				pm.setSize((int)diskspaceND.sample());
+				pm.setBandwith((int)bandwithND.sample());
+		
 				pm.setCpuPowerConsumption((int)cpuPowerND.sample());
 				pm.setMemPowerConsumption((int)memPowerND.sample());
 				pm.setNetworkPowerConsumption((int)netPowerND.sample());
 				pm.setIdleStateEnergyUtilization(0.1 * (pm.getCpuPowerConsumption() + pm.getMemPowerConsumption() + pm.getNetworkPowerConsumption()));
+
 				System.out.println(pm);
 				pms.add(pm);
 			}
