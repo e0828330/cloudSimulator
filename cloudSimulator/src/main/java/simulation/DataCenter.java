e2ef1cae7e7e709f5f10442b848a3be4e2bd15b6
Christian Proske <c.proske@spookee.de>
2014-05-11 14:39:42 +0200

    Extend DataCenter to get VM count  and avg. price per VM

--- cloudSimulator/src/main/java/simulation/DataCenter.java
+++ cloudSimulator/src/main/java/simulation/DataCenter.java
@@ -16,91 +16,102 @@ import cloudSimulator.weather.Location;
 @Data
 public class DataCenter {
 
-	private String name;
-	private List<PhysicalMachine> physicalMachines;
-	private DataCenterManagement algorithm;
+    private String name;
+    private List<PhysicalMachine> physicalMachines;
+    private DataCenterManagement algorithm;
 
-	private float energyPriceDay;
-	private float energyPriceNight;
-	private int timezoneOffset;
-	private Location location;
+    private float energyPriceDay;
+    private float energyPriceNight;
+    private int timezoneOffset;
+    private Location location;
 
-	private HashMap<VirtualMachine, Integer> migrationQueue = new HashMap<VirtualMachine, Integer>();
+    private HashMap<VirtualMachine, Integer> migrationQueue = new HashMap<VirtualMachine, Integer>();
 
-	/**
-	 * Gets called on every simulated minute. Here VM allocation and load
-	 * updating should be done
-	 */
-	public void simulate(int minute) {
-		handleMigrations(minute);
-		for (PhysicalMachine pm : physicalMachines) {
-			if (pm.isRunning()) {
-				pm.updateLoads();
-			}
-		}
-		algorithm.scaleVirtualMachines(this);
+    /**
+     * Gets called on every simulated minute. Here VM allocation and load
+     * updating should be done
+     */
+    public void simulate(int minute) {
+        handleMigrations(minute);
+        for (PhysicalMachine pm : physicalMachines) {
+            if (pm.isRunning()) {
+                pm.updateLoads();
+            }
+        }
+        algorithm.scaleVirtualMachines(this);
 		// System.out.printf("[%s] - Simulated times is %s\n", name,
-		// Utils.getCurrentTime(minute));
-	}
+        // Utils.getCurrentTime(minute));
+    }
 
-	/**
-	 * Called when a VM gets migrated to this dataCenter
-	 * 
-	 * @param vm
-	 * @param targetTime
-	 */
-	public void queueAddVirtualMachine(VirtualMachine vm, int targetTime) {
-		migrationQueue.put(vm, targetTime);
-	}
+    /**
+     * Called when a VM gets migrated to this dataCenter
+     *
+     * @param vm
+     * @param targetTime
+     */
+    public void queueAddVirtualMachine(VirtualMachine vm, int targetTime) {
+        migrationQueue.put(vm, targetTime);
+    }
 
-	/**
-	 * Handle incoming migrations by looping over the queue and find the ones
-	 * that have arrived yet (i.e the target time is reached)
-	 */
-	private void handleMigrations(int minute) {
-		Iterator<Map.Entry<VirtualMachine, Integer>> iter = migrationQueue.entrySet().iterator();
-		while (iter.hasNext()) {
-			Map.Entry<VirtualMachine, Integer> entry = iter.next();
-			if (entry.getValue() >= minute) {
-				VirtualMachine vm = entry.getKey();
-				PhysicalMachine pm = algorithm.findPMForMigration(this, vm);
-				pm.getVirtualMachines().add(vm);
-				vm.setOnline(true);
-				iter.remove();
-			}
-		}
-	}
+    /**
+     * Handle incoming migrations by looping over the queue and find the ones
+     * that have arrived yet (i.e the target time is reached)
+     */
+    private void handleMigrations(int minute) {
+        Iterator<Map.Entry<VirtualMachine, Integer>> iter = migrationQueue.entrySet().iterator();
+        while (iter.hasNext()) {
+            Map.Entry<VirtualMachine, Integer> entry = iter.next();
+            if (entry.getValue() >= minute) {
+                VirtualMachine vm = entry.getKey();
+                PhysicalMachine pm = algorithm.findPMForMigration(this, vm);
+                pm.getVirtualMachines().add(vm);
+                vm.setOnline(true);
+                iter.remove();
+            }
+        }
+    }
 
-	public float getCurrentEneryPrice(Date date) {
-		Calendar cal = Calendar.getInstance(); // creates calendar
-		cal.setTime(new Date()); // sets calendar time/date
-		cal.add(Calendar.HOUR_OF_DAY, this.timezoneOffset); // adds one hour
-		int hour = cal.get(Calendar.HOUR_OF_DAY);
-		return hour >= 8 && hour < 20 ? this.energyPriceDay : this.energyPriceNight;
-	}
+    public float getCurrentEneryPrice(Date date) {
+        Calendar cal = Calendar.getInstance(); // creates calendar
+        cal.setTime(new Date()); // sets calendar time/date
+        cal.add(Calendar.HOUR_OF_DAY, this.timezoneOffset); // adds one hour
+        int hour = cal.get(Calendar.HOUR_OF_DAY);
+        return hour >= 8 && hour < 20 ? this.energyPriceDay : this.energyPriceNight;
+    }
 
-	/**
-	 * Returns the averange prive per 
-	 * @param date
-	 * @return
-	 */
-	public double getAveragePricePerVM(Date date) {
-		return this.getPowerConsumption() / (double) this.getPhysicalMachines().size() * this.getCurrentEneryPrice(date);
-	}
-	
-	/**
-	 * Returns the PowerConsumption of the data center in Watt
-	 * 
-	 * @return
-	 */
-	public double getPowerConsumption() {
-		double result = 0.;
-		for (PhysicalMachine pm : physicalMachines) {
-			if (pm.isRunning()) {
-				result += pm.getPowerConsumption();
-			}
-		}
-		return result;
-	}
+    /**
+     * Returns the averange prive per
+     *
+     * @param date
+     * @return
+     */
+    public double getAveragePricePerVM(Date date) {
+        if(this.getVirtualMachineCount() == 0)
+            return .0;
+        return this.getPowerConsumption() / (double) this.getVirtualMachineCount() * this.getCurrentEneryPrice(date);
+    }
+
+    public int getVirtualMachineCount() {
+        int i = 0;
+        for (PhysicalMachine pm : this.getPhysicalMachines()) {
+            i += pm.getVirtualMachines().size();
+        }
+        return i;
+    }
+
+    /**
+     * Returns the PowerConsumption of the data center in Watt
+     *
+     * @return
+     */
+    public double getPowerConsumption() {
+        double result = 0.;
+        for (PhysicalMachine pm : this.getPhysicalMachines()) {
+            if (pm.isRunning()) {
+                result += pm.getPowerConsumption();
+            }
+        }
+        return result;
+    }
 
 }
